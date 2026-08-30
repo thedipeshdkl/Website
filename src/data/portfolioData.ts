eff5d22acf900f6cc2bb2dc65b012278bf0cdbbb
DIPESH DHAKAL <dipeshdhakal@outlook.com>
2026-08-30 23:11:49 +0545

    Add new cyber monogram logo as profile photo and favicon

--- src/data/portfolioData.ts
+++ src/data/portfolioData.ts
@@ -76,7 +76,7 @@ export const PERSONAL_INFO = {
     "Data Analyst"
   ],
   heroBadge: "Cybersecurity & Technology",
-  profilePicture: "/profile-photo.jpg",
+  profilePicture: "/profile-photo.png",
   location: "Kathmandu, 22 Kthamandu, Nepal",
   email: "dipeshrajdhakal@gmail.com",
 
